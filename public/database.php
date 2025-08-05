8e0f493261f2d7c4097e47055e31f249dd39c207
neotroenopawiro <302869724@student.rocmondriaan.nl>
2025-08-05 18:54:04 +0200

    new variables

--- public/database.php
+++ public/database.php
@@ -1,11 +1,14 @@
 <?php
-$host = getenv('DATABASE_SERVER');     // bijv. jouwserver.mysql.database.azure.com
-$user = getenv('DATABASE_UID');        // admin@jouwserver
-$pass = getenv('DATABASE_PASSWORD');
-$db   = getenv('DATABASE_NAME');
+$host = getenv('DATABASE_SERVER');
 $port = getenv('DATABASE_PORT') ?: 3306;
 
 $conn = mysqli_init();
-mysqli_ssl_set($conn, null, null, null, null, null);  // nodig als Azure “Enforce SSL” aan staat
-mysqli_real_connect($conn, $host, $user, $pass, $db, $port)
-  or die('DB-connectie faalt: ' . mysqli_connect_error());
+mysqli_ssl_set($conn, null, null, null, null, null); // TLS als Azure “Enforce SSL” aan heeft
+mysqli_real_connect(
+    $conn,
+    $host,
+    getenv('DATABASE_UID'),
+    getenv('DATABASE_PASSWORD'),
+    getenv('DATABASE_NAME'),
+    $port
+) or die("DB-fout: " . mysqli_connect_error());
